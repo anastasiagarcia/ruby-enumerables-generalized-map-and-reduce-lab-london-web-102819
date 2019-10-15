70696227aaa64e0886920bdd0ed50b84dfbf3cf3
Anastasia Pankina <siapankina@gmail.com>
2019-10-15 19:01:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,7 +9,15 @@ def map (array)
   return new_array
 end
 
-def reduce (array,start)
+def reduce (array,start=0)
+  number=start;
+  i=0;
+  while i<array.length do
+    start +=yield(array[i]);
+    i+=1;
+    return number
+    
+  
   #element=array[i]
   #element_modified=yield at (element)
   #new_array.push(element_modified)
